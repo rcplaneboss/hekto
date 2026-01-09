02a38577649c7737c7f0cf50d80ce7b3565df493
rcplaneboss <qosimabdulrahman0@gmail.com>
2026-01-09 17:51:40 +0100

    Fix 404 page error

--- app/global-not-found.tsx
+++ app/global-not-found.tsx
@@ -6,6 +6,10 @@ import Image from 'next/image'
 import Link from 'next/link'
 import PageHeader from '@/components/PageHeader'
 import HeaderContainer from '@/components/header/HeaderContainer'
+import { CartProvider } from '@/context/CartContext'
+import { WishlistProvider } from '@/context/WishlistContext'
+import FooterContainer from '@/components/footer/FooterContainer'
+import { auth } from '@/auth'
  
 const josefin = Josefin_Sans({ subsets: ['latin'], display: 'swap' })
  
@@ -14,10 +18,14 @@ export const metadata: Metadata = {
   description: 'The page you are looking for does not exist.',
 }
  
-export default function GlobalNotFound() {
+export default async function GlobalNotFound() {
+
+  const session = await auth();
   return (
     <html lang="en" className={josefin.className}>
       <body>
+         <WishlistProvider user={session?.user}>
+                <CartProvider>
         <HeaderContainer />
         <PageHeader title='404 Not Found' />
         <div className="flex flex-col justify-center items-center min-h-screen bg-white dark:bg-slate-950 text-slate-900 dark:text-slate-100 transition-colors duration-300 px-4">
@@ -25,6 +33,9 @@ export default function GlobalNotFound() {
         <p>Oops!This page does not exist.</p>
         <Link href="/" className="px-6 bg-pink text-white py-4">Go Back Home!</Link>
         </div>
+        <FooterContainer />
+        </CartProvider>
+        </WishlistProvider>
       </body>
     </html>
   )
